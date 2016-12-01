2f6ee7bfdb51bfc50615a84e45289d73b092f264
Jamen Marz <jamenmarz@gmail.com>
2016-12-01 05:12:43 -0700

    Add test

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var request = require('http').request;
-var routine = require('promise-routine');
-var Promise = require('any-promise');
+var got = require('got')
+var pull = require('pull-stream')
+var values = pull.values
+var asyncMap = pull.asyncMap
+var collect = pull.collect
 
-module.exports = function npmPlugin(input) {
-  return routine(function getStatus(name) {
-    return new Promise(function(resolve) {
-      request('http://registry.npmjs.org/' + name, function(resp) {
-        resolve(['npm', name, resp.statusCode === 200 ? 'taken' : 'free']);
-      }).end();
-    });
-  }, input);
-};
+module.export = hmuNpm
+
+var JSONOPTS = { json: true }
+
+function hmuNpm (pkgs, options, callback) {
+  var get = _httpsGet
+  if (options.http) get = _httpGet
+  var registry = (options.http ? 'http://' : 'https://') + 'registry.npmjs.org/'
+
+  pull(
+    values(pkgs),
+    asyncMap(function (pkg, done) {
+      got(registry + pkg + '/latest', JSONOPTS).then(function (res) {
+        var isTaken = res.statusCode === 200 && !res.body.unpublished
+        done(null, pkg + ' ' + (isTaken ? 'taken' : 'free'))
+      }, done)
+    }),
+    collect(callback)
+  )
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# hmu-npm
+
+> A HMU plugin for checking npm packages
+
+## Installation
+
+```sh
+$ npm install --save hmu-npm
+```
+
+## Usage
+
+### `hmuNpm()`
+
+Does something
+
+## License
+
+MIT © [Jamen Marz](https://git.io/jamen)
+
+---
+
+[![version](https://img.shields.io/npm/v/hmu-npm.svg?style=flat-square)][package] [![travis](https://img.shields.io/travis/jamen/hmu-npm.svg?style=flat-square)](https://travis-ci.org/jamen/hmu-npm) [![downloads](https://img.shields.io/npm/dt/hmu-npm.svg?style=flat-square)][package] [![license](https://img.shields.io/npm/l/express.svg?style=flat-square)][package] [![follow](https://img.shields.io/github/followers/jamen.svg?style=social&label=Follow)](https://github.com/jamen)
+
+[package]: https://npmjs.org/package/hmu-npm
